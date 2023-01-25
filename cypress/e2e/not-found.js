5812c4b842ad880130763a9f5c1c08bc5c82691d
msmarjorine <marjorine13@gmail.com>
2023-01-25 16:31:34 +0300

    Added test for a Not Found page

---
+++ cypress/e2e/not-found.js
@@ -0,0 +1,13 @@
+/// <reference types = "cypress"/>
+
+describe('Check the 404 page', () => {
+    it('The not found page should load', () => {
+        cy.visit('/something', { failOnStatusCode: false })
+        cy.request({ url: '/something', failOnStatusCode: false }).its('status').should('eq', 404)
+        cy.title().should('eq', 'ой')
+        cy.verifyGlobalElements()
+        cy.get('#czterysta1').should('have.css', 'fontSize', '60px')
+        cy.get('#lagente').should('be.visible')
+    })
+
+})
