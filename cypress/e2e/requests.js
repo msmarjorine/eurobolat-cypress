832409922cae40ed5fac1f79cbcd92f223dc4d90
msmarjorine <marjorine13@gmail.com>
2023-02-22 18:06:21 +0300

    Add requests.js

---
+++ cypress/e2e/requests.js
@@ -0,0 +1,14 @@
+/// <reference types = "cypress"/>
+
+describe("Verify http requests", () => {
+  it("The homepage request should be successful", () => {
+    cy.request({
+      method: "GET",
+      url: "/",
+    }).then((response) => {
+      expect(response.status).to.eql(200);
+      expect(response.headers).to.have.property("server", "awex");
+      expect(response.duration).to.be.lt(1000);
+    });
+  });
+});
