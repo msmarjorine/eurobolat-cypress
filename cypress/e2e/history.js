b3a823018a37841a9cbe1a8ddab36361f60193c5
msmarjorine <marjorine13@gmail.com>
2023-03-21 12:39:35 +0300

    Add test for Disqus iframe

--- cypress/e2e/history.js
+++ cypress/e2e/history.js
@@ -25,4 +25,33 @@ describe("Check the history page", () => {
             expect(request.headers).to.have.property('referer', 'https://www.instagram.com/')
         })*/
   });
+  it.only("The Disqus iframe should load successfully", () => {
+    const testComment = "Checking the ability to write comments";
+    cy.intercept({
+      method: "GET",
+      url: "https://disqus.com/api/3.0/forums/**",
+      resourceType: "xhr",
+    }).as("disqus");
+    cy.visit("/history.html");
+
+    cy.wait("@disqus").then(({ request, response }) => {
+      expect(response.statusCode).to.eq(200);
+      expect(request.headers).to.have.property("host", "disqus.com");
+      expect(response.body.response.id).to.eq("eurobolat");
+      expect(response.body.response.category).to.eq("Entertainment");
+    });
+    //
+    cy.get("iframe[id^='dsq-app']").then((disqus) => {
+      const body = disqus.contents().find("body");
+      cy.wrap(body).as("disqusFrame");
+    });
+    cy.get("@disqusFrame").find("#reactions__container").should("be.visible");
+    cy.get("@disqusFrame").find("#conversation").should("be.visible");
+    cy.get("@disqusFrame")
+      .find("div.textarea")
+      .type(testComment, { force: true });
+    cy.get("@disqusFrame")
+      .find("div.textarea > p")
+      .should("have.text", testComment);
+  });
 });
